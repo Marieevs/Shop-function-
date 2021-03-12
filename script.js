b64425363ca4742ccb7f5cd2b1f9fecfa0df9100
Marieevs <m.yauseichyk@gmail.com>
2021-03-12 13:39:51 +0300

    Passing parameters to a function

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const PRODUCT_TYPE = {
     CLOTHES: 4,
     BAKERY: 5,
 }
+
 const data = [
     {
         name: 'Milk',
@@ -64,27 +65,89 @@ const data = [
     },
 ]
 
+const korona = [
+    {
+        name: 'Yogurt',
+        price: 20,
+        quantity: 1,
+        type: PRODUCT_TYPE.MILK_FOOD,
+    },
+    {
+        name: 'Pineapple',
+        price: 10,
+        quantity: 3,
+        type: PRODUCT_TYPE.FRUIT,
+    },
+    {
+        name: 'Cheese',
+        price: 12,
+        quantity: 1,
+        type: PRODUCT_TYPE.MILK_FOOD,
+    },
+    {
+        name: 'Cake',
+        price: 23,
+        quantity: 1,
+        type: PRODUCT_TYPE.BAKERY,
+    },
+    {
+        name: 'Strawberry',
+        price: 12,
+        quantity: 3,
+        type: PRODUCT_TYPE.FRUIT,
+    },
+    {
+        name: 'Pants',
+        price: 200,
+        quantity: 1,
+        type: PRODUCT_TYPE.CLOTHES,
+    },
+    {
+        name: 'Dress',
+        price: 400,
+        quantity: 1,
+        type: PRODUCT_TYPE.CLOTHES,
+    },
+    {
+        name: 'Mushrooms',
+        price: 15,
+        quantity: 5,
+        type: PRODUCT_TYPE.VEGETABLE,
+    },
+    {
+        name: 'Potato',
+        price: 10,
+        quantity: 4,
+        type: PRODUCT_TYPE.VEGETABLE,
+    },
+]
+
 
 let newList;
+let productType;
+let i;
 
-function filterOfType() {
-    let productType = prompt('По какому типу продуктов Вы хотите отфильтровать все продукты? (Выберите числа от 1 до 5)');
-    newList = [];
-    for (let i = 0; i < data.length; i++) {
-        let {name, price, quantity, type} = data[i];
-        if (type == productType) {
-            (newList.push(data[i])) ;
-        }
-    }
+function getProductType(){
+    productType = prompt('По какому типу продуктов Вы хотите отфильтровать все продукты? (Выберите числа от 1 до 5)');
 }
-
 
 function displayAll() {
     console.log(newList);
 }
 
-filterOfType()
+function filterOfType(inputArray) {
+    newList = [];
+    for ( i = 0; i < inputArray.length; i++) {
+        let {name, price, quantity, type} = inputArray[i];
+        if (type == productType) {
+            (newList.push(inputArray[i])) ;
+        }
+    }
+}
+getProductType()
+filterOfType(korona)
 displayAll()
-filterOfType()
+getProductType()
+filterOfType(korona)
 displayAll()
 
